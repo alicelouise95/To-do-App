a002491b8ebcd20c272b066d27ad74f20195eb78
alice <alicelouisew95@gmail.com>
2023-10-12 11:31:11 +0200

    Todo

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, FlatList, TextInput, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import TodoScreen from "./TodoScreen";
+import TodoDetail from "./TodoDetail";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="todo">
+        <Stack.Screen name="todo" component={TodoScreen} />
+        <Stack.Screen name="TodoDetail" component={TodoDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ TodoDetail.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, FlatList, TextInput, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export default function TodoDetail({ route, navigation }) {
+  const [todotitle, setTodotitle] = useState(route.params.todoitem.key);
+
+  return (
+    <View>
+      <Text>TODO DETAIL</Text>
+      <TextInput
+        value={todotitle}
+        onChangeText={setTodotitle}
+        placeholder="Change the title"
+      />
+
+      <Button
+        title="Save"
+        onPress={() => {
+          navigation.navigate({
+            name: "todo",
+            params: { todoname: todotitle, rownumb: route.params.rownumb },
+            merge: true,
+          });
+        }}
+      />
+    </View>
+  );
+}
